cb7ba8a61242af02f192c675ceaa5e75dd9d4e96
shivyelave <syelave20ite@student.mes.ac.in>
2024-09-04 10:17:41 +0530

    [Shiv] Add: whole todo list project.

---
+++ base/models.py
@@ -0,0 +1,23 @@
+from tkinter import Widget
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime , date
+
+# Create your models here.
+
+
+class Task(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    due = models.DateField("Due(mm/dd/yyyy)",auto_now_add=False, auto_now=False, blank=True)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        order_with_respect_to = 'user'
